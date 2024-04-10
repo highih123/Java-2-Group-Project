d73c8233cd4f8d236225dbd870292d5d4a2324cb
highih123 <142607511+highih123@users.noreply.github.com>
2024-04-10 19:44:39 -0400

    Add files via upload

---
+++ autominder/ExpensesController.java
@@ -0,0 +1,50 @@
+package autominder;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class ExpensesController {
+
+    @FXML
+    private Button LogExpensesButton;
+
+    @FXML
+    private TextField gasExpensesTextField;
+
+    @FXML
+    private TextField motorMaintenanceExpenses;
+
+    @FXML
+    private TextField tireExpensesTextField;
+
+    @FXML
+    private TextField totalExpensesTextField;
+
+    @FXML
+    void handleGasExpensesTextField(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleLogExpensesButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleMotorMaintenanceExpenses(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleTireExpensesTextField(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleTotalExpensesTextField(ActionEvent event) {
+
+    }
+
+}
